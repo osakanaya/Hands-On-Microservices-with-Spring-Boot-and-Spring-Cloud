3576b85014fb35ed0acac95b119c714961e61f3f
sashida <sashida@primagest.co.jp>
2020-12-17 00:30:51 +0900

    p.173 - product-service

--- chapter03/microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
+++ chapter03/microservices/product-service/src/main/java/se/magnus/microservices/core/product/services/ProductServiceImpl.java
@@ -3,10 +3,13 @@ package se.magnus.microservices.core.product.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.magnus.api.core.product.Product;
 import se.magnus.api.core.product.ProductService;
+import se.magnus.microservices.core.product.persistence.ProductEntity;
+import se.magnus.microservices.core.product.persistence.ProductRepository;
 import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.exceptions.NotFoundException;
 import se.magnus.util.http.ServiceUtil;
@@ -18,9 +21,13 @@ public class ProductServiceImpl implements ProductService {
 	private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
 	
 	private final ServiceUtil serviceUtil;
+	private final ProductRepository repository;
+	private final ProductMapper mapper;
 	
 	@Autowired
-	public ProductServiceImpl(ServiceUtil serviceUtil) {
+	public ProductServiceImpl(ProductRepository repository, ProductMapper mapper, ServiceUtil serviceUtil) {
+		this.repository = repository;
+		this.mapper = mapper;
 		this.serviceUtil = serviceUtil;
 	}
 	
@@ -29,9 +36,31 @@ public class ProductServiceImpl implements ProductService {
 		LOG.debug("/product return the found product for product={}", productId);
 
 		if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
-		if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
+
+		ProductEntity entity = repository.findByProductId(productId)
+			.orElseThrow(() -> new NotFoundException("No product found for productId: " + productId));
 		
-		return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+		Product response = mapper.entityToApi(entity);
+		response.setServiceAddress(serviceUtil.getServiceAddress());
+		
+		return response;
+	}
+
+	@Override
+	public Product createProduct(Product body) {
+		try {
+			ProductEntity entity = mapper.apiToEntity(body);
+			ProductEntity newEntity = repository.save(entity);
+			
+			return mapper.entityToApi(newEntity);
+		} catch (DuplicateKeyException ex) {
+			throw new InvalidInputException("Duplicate Key, Product Id: " + body.getProductId());
+		}
+	}
+
+	@Override
+	public void deleteProduct(int productId) {
+		repository.findByProductId(productId).ifPresent(e -> repository.delete(e));
 	}
 
 }
--- chapter03/microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
+++ chapter03/microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
@@ -1,45 +1,83 @@
 package se.magnus.microservices.core.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import se.magnus.api.core.product.Product;
+import se.magnus.microservices.core.product.persistence.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+import static reactor.core.publisher.Mono.just;
+import static org.springframework.http.HttpStatus.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"spring.data.mongodb.port:0"})
 class ProductServiceApplicationTests {
 
 	@Autowired
 	private WebTestClient client;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@BeforeEach
+	public void setupDb() {
+		repository.deleteAll();
+	}
 	
 	@Test
 	public void getProductById() {
 		
 		int productId = 1;
 		
-		client.get()
-			.uri("/product/" + productId)
-			.accept(APPLICATION_JSON)
-			.exchange()
-			.expectStatus().isOk()
-			.expectHeader().contentType(APPLICATION_JSON)
-			.expectBody()
+		postAndVerifyProduct(productId, OK);
+		assertTrue(repository.findByProductId(productId).isPresent());
+		
+		getAndVeirfyProduct(productId, OK)
 			.jsonPath("$.productId").isEqualTo(productId);
 	}
 	
 	@Test
+	public void duplicateError() {
+		
+		int productId = 1;
+		
+		postAndVerifyProduct(productId, OK);
+		assertTrue(repository.findByProductId(productId).isPresent());
+		
+		postAndVerifyProduct(productId, UNPROCESSABLE_ENTITY)
+			.jsonPath("$.path").isEqualTo("/product")
+			.jsonPath("$.message").isEqualTo("Duplicate Key, Product Id: " + productId);
+		
+	}
+	
+	@Test
+	public void deleteProduct() {
+
+		int productId = 1;
+		
+		postAndVerifyProduct(productId, OK);
+		assertTrue(repository.findByProductId(productId).isPresent());
+
+		deleteAndVerifyProduct(productId, OK);
+		assertFalse(repository.findByProductId(productId).isPresent());
+		
+		deleteAndVerifyProduct(productId, OK);
+	}
+	
+	@Test
 	public void getProductInvalidParameterString() {
-		client.get()
-			.uri("/product/no-integer")
-			.accept(APPLICATION_JSON)
-			.exchange()
-			.expectStatus().isEqualTo(BAD_REQUEST)
-			.expectHeader().contentType(APPLICATION_JSON)
-			.expectBody()
+		
+		getAndVeirfyProduct("/no-integer", BAD_REQUEST)
 			.jsonPath("$.path").isEqualTo("/product/no-integer")
 	        .jsonPath("$.message").isEqualTo("Type mismatch.");
 	}
@@ -48,14 +86,8 @@ class ProductServiceApplicationTests {
 	public void getProductNotFound() {
 		
 		int productIdNotFound = 13;
-		
-		client.get()
-			.uri("/product/" + productIdNotFound)
-			.accept(APPLICATION_JSON)
-			.exchange()
-			.expectStatus().isNotFound()
-			.expectHeader().contentType(APPLICATION_JSON)
-			.expectBody()
+
+		getAndVeirfyProduct(productIdNotFound, NOT_FOUND)
 			.jsonPath("$.path").isEqualTo("/product/" + productIdNotFound)
 			.jsonPath("$.message").isEqualTo("No product found for productId: " + productIdNotFound);
 	}
@@ -65,15 +97,45 @@ class ProductServiceApplicationTests {
 		
 		int productIdInvalid = -1;
 		
-		client.get()
-			.uri("/product/" + productIdInvalid)
-			.accept(APPLICATION_JSON)
-			.exchange()
-			.expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
-			.expectHeader().contentType(APPLICATION_JSON)
-			.expectBody()
+		getAndVeirfyProduct(productIdInvalid, UNPROCESSABLE_ENTITY)
 			.jsonPath("$.path").isEqualTo("/product/" + productIdInvalid)
 			.jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+	}
+	
+	private WebTestClient.BodyContentSpec getAndVeirfyProduct(int productId, HttpStatus expectedStatus) {
+		return getAndVeirfyProduct("/" + productId, expectedStatus);
+	}
+	
+	private WebTestClient.BodyContentSpec getAndVeirfyProduct(String productIdPath, HttpStatus expectedStatus) {
+		return client.get()
+			.uri("/product" + productIdPath)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isEqualTo(expectedStatus)
+			.expectHeader().contentType(APPLICATION_JSON)
+			.expectBody();
+	}
+	
+	private WebTestClient.BodyContentSpec postAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		Product product = new Product(productId, "Name " + productId, productId, "SA");
+		
+		return client.post()
+			.uri("/product")
+			.body(just(product), Product.class)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isEqualTo(expectedStatus)
+			.expectHeader().contentType(APPLICATION_JSON)
+			.expectBody();
+	}
+	
+	private WebTestClient.BodyContentSpec deleteAndVerifyProduct(int productId, HttpStatus expecHttpStatus) {
+		return client.delete()
+			.uri("/product/" + productId)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isEqualTo(expecHttpStatus)
+			.expectBody();
 	}
 
 }
