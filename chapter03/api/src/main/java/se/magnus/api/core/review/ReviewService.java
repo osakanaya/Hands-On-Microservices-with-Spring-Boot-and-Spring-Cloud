2b5c37e589fd3537952c9c28d8f59fa9d22985a3
sashida <sashida@primagest.co.jp>
2020-12-17 10:23:59 +0900

    p.173 - review-service

--- chapter03/api/src/main/java/se/magnus/api/core/review/ReviewService.java
+++ chapter03/api/src/main/java/se/magnus/api/core/review/ReviewService.java
@@ -2,7 +2,10 @@ package se.magnus.api.core.review;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewService {
@@ -11,5 +14,15 @@ public interface ReviewService {
 		value = "/review",
 		produces = "application/json"
 	)
-	List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);	
+	List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+	
+	@PostMapping(
+		value = "/review",
+		consumes = "application/json",
+		produces = "application/json"
+	)
+	Review createReview(@RequestBody Review body);
+	
+	@DeleteMapping(value = "/review")
+	void deleteReviews(@RequestParam(value = "productId", required = true) int productId);
 }
