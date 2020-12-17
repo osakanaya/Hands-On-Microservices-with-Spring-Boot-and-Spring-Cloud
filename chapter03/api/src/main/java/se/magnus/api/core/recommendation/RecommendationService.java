622cfa68d4ec272556f14144aa77eb8b2bc61694
sashida <sashida@primagest.co.jp>
2020-12-17 09:52:33 +0900

    p.173 - recommendation-service

--- chapter03/api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
+++ chapter03/api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
@@ -2,7 +2,10 @@ package se.magnus.api.core.recommendation;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface RecommendationService {
@@ -12,4 +15,14 @@ public interface RecommendationService {
 		produces = "application/json"
 	)
 	List<Recommendation> getRecommendations(@RequestParam(value = "productId", required = true) int productId);
+	
+	@PostMapping(
+		value = "/recommendation",
+		consumes = "application/json",
+		produces = "application/json"
+	)
+	Recommendation createRecommendation(@RequestBody Recommendation body);
+	
+	@DeleteMapping(value = "/recommendation")
+	void deleteRecommendations(@RequestParam(value = "productId", required = true) int productId);
 }
--- chapter03/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
+++ chapter03/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImpl.java
@@ -1,15 +1,17 @@
 package se.magnus.microservices.core.recommendation.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.magnus.api.core.recommendation.Recommendation;
 import se.magnus.api.core.recommendation.RecommendationService;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationEntity;
+import se.magnus.microservices.core.recommendation.persistence.RecommendationRepository;
 import se.magnus.util.exceptions.InvalidInputException;
 import se.magnus.util.http.ServiceUtil;
 
@@ -18,10 +20,14 @@ public class RecommendationServiceImpl implements RecommendationService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RecommendationServiceImpl.class);
 	
+	private final RecommendationRepository repository;
+	private final RecommendationMapper mapper;
 	private final ServiceUtil serviceUtil;
 	
 	@Autowired
-	public RecommendationServiceImpl(ServiceUtil serviceUtil) {
+	public RecommendationServiceImpl(RecommendationRepository repository, RecommendationMapper mapper, ServiceUtil serviceUtil) {
+		this.repository = repository;
+		this.mapper = mapper;
 		this.serviceUtil = serviceUtil;
 	}
 	
@@ -29,20 +35,37 @@ public class RecommendationServiceImpl implements RecommendationService {
 	public List<Recommendation> getRecommendations(int productId) {
 		if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
 		
-		if (productId == 113) {
-			LOG.debug("No recommendations found for productId: {}", productId);
+		LOG.debug("No recommendations found for productId: {}", productId);
 
-			return new ArrayList<Recommendation>();
-		}
+		List<RecommendationEntity> entityList = repository.findByProductId(productId);
+		List<Recommendation> list = mapper.entityListToApiList(entityList);
+		list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 		
-		List<Recommendation> list = new ArrayList<Recommendation>();
-        list.add(new Recommendation(productId, 1, "Author 1", 1, "Content 1", serviceUtil.getServiceAddress()));
-        list.add(new Recommendation(productId, 2, "Author 2", 2, "Content 2", serviceUtil.getServiceAddress()));
-        list.add(new Recommendation(productId, 3, "Author 3", 3, "Content 3", serviceUtil.getServiceAddress()));
-        
-        LOG.debug("/recommendation response size: {}", list.size());
-        
-        return list;
+		LOG.debug("getRecommendations: response size: {}", list.size());
+		
+		return list;		
 	}
 
+	@Override
+	public Recommendation createRecommendation(Recommendation body) {
+		try {
+			RecommendationEntity entity = mapper.apiToEntity(body);
+			RecommendationEntity newEntity = repository.save(entity);
+			
+			LOG.debug("createRecommendation: created a recommendation entity: {}/{}", 
+					body.getProductId(), body.getRecommendationId());
+			
+			return mapper.entityToApi(newEntity);
+			
+		} catch (DuplicateKeyException ex) {
+			throw new InvalidInputException("Duplicate Key, Product Id: " + body.getProductId() + ", Recommendation Id: " + body.getRecommendationId());
+		}
+	}
+
+	@Override
+	public void deleteRecommendations(int productId) {
+		LOG.debug("deleteRecommnedations: tries to delete recommendations for the product with productId: {}", productId);
+		
+		repository.deleteAll(repository.findByProductId(productId));
+	}
 }
