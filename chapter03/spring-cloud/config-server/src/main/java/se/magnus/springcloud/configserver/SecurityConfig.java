7dc651e94ee1c28a5e7f3b0cd2a93c3776d620a5
sashida <sashida@primagest.co.jp>
2021-01-03 02:31:18 +0900

    p.450

--- chapter03/spring-cloud/config-server/src/main/java/se/magnus/springcloud/configserver/SecurityConfig.java
+++ chapter03/spring-cloud/config-server/src/main/java/se/magnus/springcloud/configserver/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.csrf().disable()
 			.authorizeRequests()
+				.antMatchers("/actuator/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.httpBasic();
